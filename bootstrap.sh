383806020ca17411601fd03bcd9cdcea799df83c
偏右 <afcstar@163.com>
2013-03-09 01:12:18 +0800

    fix bootstrap shell and doc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,6 @@
 
 CWD=$(dirname "$0")
 THEME="$HOME/.spm/themes/alice"
-JSC="https://raw.github.com/lepture/nico/master/scripts/node-jscoverage.rb"
 
 cd $CWD
 
@@ -23,38 +22,9 @@ else
     cd $CWD
 fi
 
-if ! which jscoverage > /dev/null; then
-    echo "install jscoverage"
-
-    git clone https://github.com/visionmedia/node-jscoverage.git .jscoverage
-    cd .jscoverage
-    ./configure --prefix=/usr/local
-    make
-    make install
-    cd $CWD
-    rm -fr .jscoverage
-else
-    echo "you have installed jscoverage"
-fi
-
 if ! which jade > /dev/null; then
     echo "install jade"
     npm install jade -g
 else
     echo "you have installed jade"
-fi
-
-if ! which mocha > /dev/null; then
-    echo "install mocha"
-    npm install mocha -g
-else
-    echo "you have installed mocha"
-fi
-
-
-if ! which mocha-phantomjs > /dev/null; then
-    echo "install mocha phantomjs"
-    npm install mocha-phantomjs -g
-else
-    echo "you have installed mocha-phantomjs"
 fi
